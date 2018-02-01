c08ba48129d13112b70b0dbabd0ae375501a33fd
5-4 <github email address>
2018-01-31 18:57:24 -0600

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,33 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+  	Boat.all.limit(5)
+  end
+
+  def self.dinghy
+  	Boat.where("length < 20")
+  end
+
+  def self.ship
+  	Boat.where("length >= 20")
+  end
+
+  def self.last_three_alphabetically
+  	Boat.all.order(name: :desc).limit(3)
+  end
+
+  def self.without_a_captain
+  	Boat.where("captain_id IS NULL")
+  end
+
+  def self.sailboats
+  	Boat.joins(:boat_classifications).joins("JOIN classifications ON boat_classifications.classification_id = classifications.id").where("classifications.name = 'Sailboat'")
+  end
+
+  def self.with_three_classifications
+  	Boat.joins(:boat_classifications).select("*, COUNT(*)").group("boat_classifications.boat_id").having("COUNT(*) = 3")
+  end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,24 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.find_by_boat_type(type)
+  	Captain.joins(:boats).joins("JOIN boat_classifications ON boats.id = boat_classifications.boat_id").joins("JOIN classifications ON boat_classifications.classification_id = classifications.id").where("classifications.name = ?", type).group(:captain_id)
+  end
+
+  def self.catamaran_operators
+  	self.find_by_boat_type('Catamaran')
+  end
+
+  def self.sailors
+  	self.find_by_boat_type('Sailboat')
+  end
+
+  def self.talented_seamen
+  	Captain.joins(:boats).joins("JOIN boat_classifications ON boats.id = boat_classifications.boat_id").joins("JOIN classifications ON boat_classifications.classification_id = classifications.id").select("captains.name, captains.id").distinct.where("classifications.name = 'Sailboat' or classifications.name = 'Motorboat'").group("classifications.id")
+  end
+
+  def self.non_sailors
+  		Captain.joins(:boats).joins("JOIN boat_classifications ON boats.id = boat_classifications.boat_id").joins("JOIN classifications ON boat_classifications.classification_id = classifications.id").where.("classifications.name = 'Sailboat'").group(:captain_id)
+  end
+
 end
